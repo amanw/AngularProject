f228ff9eadfc739e77d312660c55f39e362de0d8
amanw <aman.wangde@gmail.com>
2018-12-11 02:29:28 -0500

    Final changes with server and deployment

--- server/config/dev.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    DB_URI: 'mongodb://admin:admin123@ds063946.mlab.com:63946/angular-web-dev',
-    SECRET: 'fsdfsdfsdgsdg34534fdvd',
-    AWS_SECRET_ACCESS_KEY:'/MX0G1jwaY81DrfMmHX6W3cUwAia24O1mba4mVmP',
-    AWS_ACCESS_KEY_ID:'AKIAIPHWDVYG7UP7QQMQ'
-  }
